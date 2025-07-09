9814ffd699ae849788a05e9e9a9ac1b4886941ed
Daniel Vergara <vergara.daniel@correounivalle.edu.co>
2025-07-09 00:05:35 -0500

    Agregados listeners de mouse y escenas básicas en los modelos 3D, en la página de cirrosis

--- src/pages/organ/LiverModel.jsx
+++ src/pages/organ/LiverModel.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Center, OrbitControls, PerspectiveCamera, useGLTF } from '@react-three/drei';
 
@@ -11,6 +11,17 @@ function Model({ path, scale = 5 }) {
       modelRef.current.rotation.y += 0.005;
     }
   });
+
+  useEffect(() => {
+    if(scene){
+      scene.traverse((child) => {
+        if(child.isMesh){
+          child.castShadow = true;
+          child.recieveShadow = true;
+        }
+      })
+    }
+  }, [scene])
 
   return (
     <Center>
@@ -26,11 +37,19 @@ function Model({ path, scale = 5 }) {
 
 export default function LiverModelHome({ modelPath, scale }) {
   return (
-    <Canvas style={{ width: '800px', height: '400px' }}>
+    <Canvas style={{ width: '800px', height: '400px' }} shadows>
       <PerspectiveCamera makeDefault position={[0, 0, 4]} fov={25} />
       <ambientLight intensity={0.7} />
-      <directionalLight position={[5, 5, 5]} intensity={1} />
-      <directionalLight position={[-5, -5, -5]} intensity={0.5} color="#ffffff" />
+      <directionalLight position={[5, 5, 5]} intensity={1} castShadow/>
+      <directionalLight position={[-5, -5, -5]} intensity={0.5} color="#ffffff"/>
+      <mesh
+          receiveShadow
+          rotation={[-Math.PI / 2, 0, 0]}
+          position={[0, -0.5, 0]}
+        >
+           <planeGeometry args={[5, 5]} />
+          <meshStandardMaterial color="#e0e0e0" />
+        </mesh>
       <OrbitControls />
       {/* Aquí puedes pasar el scale que necesites */}
       <Model path={modelPath} scale={scale} />
